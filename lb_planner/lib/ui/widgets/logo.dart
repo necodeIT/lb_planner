1700c3f279a7052d1a93bd7fdec65ebeee607897
BuildTools <bmceachnie@student.tgm.ac.at>
2021-08-28 19:57:08 +0200

    Update logo.dart

--- lb_planner/lib/ui/widgets/logo.dart
+++ lb_planner/lib/ui/widgets/logo.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:lb_planner/ui.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class NcLogo extends StatelessWidget {
   const NcLogo({
