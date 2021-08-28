2c7997a35695d8e864ebd69c51fb4da89af50d8a
BuildTools <bmceachnie@student.tgm.ac.at>
2021-08-28 18:10:02 +0200

    Update confirmDialog.dart

--- lb_planner/lib/ui/widgets/confirmDialog.dart
+++ lb_planner/lib/ui/widgets/confirmDialog.dart
@@ -2,18 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:lb_planner/ui.dart';
 
 class NcConfirmDialog extends StatelessWidget {
-  NcConfirmDialog({Key? key, required this.title, required this.body, this.confirmText = "Confirm", this.cancelText = "Cancel", required this.onConfirm, required this.onCancel, this.width = 100}) : super(key: key) {
+  NcConfirmDialog(
+      {Key? key, this.title = "", required this.body, this.confirmText = "Confirm", this.cancelText = "Cancel", required this.onConfirm, required this.onCancel, this.width = defaultWidth, this.label, this.buttonWidth = defaultButtonWidth})
+      : super(key: key) {
     confirmOnly = false;
   }
-  NcConfirmDialog.ok({Key? key, required this.title, required this.body, required this.onConfirm, this.confirmText = "OK", this.width = 100}) : super(key: key) {
+  NcConfirmDialog.ok({Key? key, required this.title, required this.body, required this.onConfirm, this.confirmText = "OK", this.width = defaultWidth, this.label, this.buttonWidth = defaultButtonWidth}) : super(key: key) {
     confirmOnly = true;
-    onCancel = null;
   }
 
   final String title;
+  final Widget? label;
   final Widget body;
   final String confirmText;
-  final double? width;
+  final double width;
+  final double buttonWidth;
   late final String? cancelText;
   late final bool confirmOnly;
 
@@ -21,26 +24,33 @@ class NcConfirmDialog extends StatelessWidget {
   late final Function()? onCancel;
 
   static const double padding = 20;
+  static const double defaultWidth = 400;
+  static const double defaultButtonWidth = 100;
 
   @override
   Widget build(BuildContext context) {
+    if (title.isEmpty && label == null) throw ArgumentError("Please define either a label or a title!");
+
     return AlertDialog(
-      title: NcTitleText(title, fontSize: 30),
+      title: label ?? NcTitleText(title, fontSize: 30),
       titlePadding: EdgeInsets.all(padding),
       buttonPadding: EdgeInsets.only(left: padding, right: padding),
       contentPadding: EdgeInsets.only(bottom: padding, left: padding, right: padding),
-      content: body,
+      content: Container(
+        child: body,
+        width: width,
+      ),
       actions: <Widget>[
         Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            if (!confirmOnly) NcButton.cancel(text: cancelText ?? "", onTap: onCancel ?? () {}, width: width),
+            if (!confirmOnly) NcButton.cancel(text: cancelText ?? "", onTap: onCancel ?? () {}, width: buttonWidth),
             NcSpacing.width(),
-            NcButton(text: confirmText, onTap: onConfirm, width: width),
+            NcButton(text: confirmText, onTap: onConfirm, width: buttonWidth),
           ],
         )
       ],
-      backgroundColor: NcThemes.current.tertiaryColor,
+      backgroundColor: NcThemes.current.primaryColor,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(ncRadius)),
       ),
