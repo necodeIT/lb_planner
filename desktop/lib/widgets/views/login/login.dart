c9e8bc9460d5f65f79c7c627e1bcb68b19972a41
BuildTools <bmceachnie@student.tgm.ac.at>
2021-08-29 02:31:33 +0200

    Update login.dart

--- desktop/lib/widgets/views/login/login.dart
+++ desktop/lib/widgets/views/login/login.dart
@@ -1,34 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:lb_planner/ui.dart';
 import 'svg/wave.dart';
 
 class Login extends StatelessWidget {
-  const Login({Key? key}) : super(key: key);
+  Login({Key? key}) : super(key: key);
+
+  String username = "";
+  String password = "";
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: NcThemes.current.primaryColor,
       padding: EdgeInsets.zero,
-      // child: Row(
-      //   mainAxisAlignment: MainAxisAlignment.start,
-      //   crossAxisAlignment: CrossAxisAlignment.end,
-      //   children: [
-      //     Container(
-      //       // alignment: Alignment.topLeft,
-      //       child: NcVectorImage(
-      //         code: wave,
-      //         width: 1000,
-      //         // height: double.infinity,
-      //       ),
-      //     ),
-      //   ],
-      // ),
-      child: NcVectorImage(
-        code: wave,
-        aliignment: Alignment.bottomLeft,
-        width: double.infinity,
-        height: double.infinity,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          NcVectorImage(
+            code: wave,
+            aliignment: Alignment.bottomLeft,
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 50),
+            alignment: Alignment.center,
+            width: 350,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                NcLogo(
+                  width: 100,
+                ),
+                NcMaterialInputField(
+                  onValueChanged: (value) => username = value,
+                  placeholder: 'Username',
+                  prefixIcon: Icon(
+                    FontAwesome5Solid.user,
+                    color: NcThemes.current.textColor,
+                  ),
+                ),
+                NcMaterialInputField(
+                  onValueChanged: (value) => password = value,
+                  placeholder: 'Passsword',
+                  prefixIcon: Icon(
+                    FontAwesome5Solid.lock,
+                    color: NcThemes.current.textColor,
+                  ),
+                ),
+                NcButton(
+                  text: "Login",
+                  onTap: () {},
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
