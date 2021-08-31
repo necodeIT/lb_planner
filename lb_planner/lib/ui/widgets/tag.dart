c9746d90c0798ddb3b9380ca8ba9a75dec9c541b
BuildTools <bmceachnie@student.tgm.ac.at>
2021-08-31 03:58:06 +0200

    Update tag.dart

--- lb_planner/lib/ui/widgets/tag.dart
+++ lb_planner/lib/ui/widgets/tag.dart
@@ -2,14 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:lb_planner/ui.dart';
 
 class NcTag extends StatelessWidget {
-  const NcTag(
-      {Key? key,
-      required this.text,
-      required this.backgroundColor,
-      this.width,
-      this.height = 25,
-      this.fontSize})
-      : super(key: key);
+  const NcTag({Key? key, required this.text, required this.backgroundColor, this.width, this.height = 25, this.fontSize}) : super(key: key);
   final Color backgroundColor;
   final String text;
   final double? width;
@@ -24,8 +17,7 @@ class NcTag extends StatelessWidget {
       padding: EdgeInsets.only(left: padding, right: padding),
       width: width,
       height: height,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(7), color: backgroundColor),
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(7), color: backgroundColor),
       child: Center(
         child: NcTitleText(
           text,
