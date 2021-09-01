c176146f19518055d6066f2ab093d07ab40b80ab
BuildTools <bmceachnie@student.tgm.ac.at>
2021-09-01 14:47:18 +0200

    Update general.dart

--- desktop/lib/widgets/views/settings/general.dart
+++ desktop/lib/widgets/views/settings/general.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:lb_planner/ui.dart';
 
 class SettingsGeneralItem extends StatelessWidget {
