d5fbbaf938c4f8a058cf9f1f9f80b907e1dec979
bmceachnie <60017181+bmceachnie@users.noreply.github.com>
2021-09-05 16:38:23 +0200

    doggirl

--- desktop/lib/main.dart
+++ desktop/lib/main.dart
@@ -20,8 +20,7 @@ void main() {
   runApp(app);
 }
 
-final app = MaterialApp(
-    home: App(), title: "LB Planner", scrollBehavior: NcScrollBehavior());
+final app = MaterialApp(home: App(), title: "LB Planner", scrollBehavior: NcScrollBehavior());
 
 class App extends StatefulWidget {
   App({Key? key}) : super(key: key);
@@ -33,26 +32,10 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    NcThemes.onCurrentThemeChange = () =>
-        setState(() => User.current.settings.theme = NcThemes.current.name);
+    NcThemes.onCurrentThemeChange = () => setState(() => User.current.settings.theme = NcThemes.current.name);
     return Scaffold(
       backgroundColor: NcThemes.current.secondaryColor,
-      //body: Home(),
-      body: Center(
-          // child: CalenderDayItem(
-          //   text: '01',
-          //   children: [
-          //     CalendarItem(id: 0),
-          //     CalendarItem(id: 0),
-          //     CalendarItem(id: 0),
-          //   ],
-          // ),
-          // child: CalenderSwitch(
-          //   text: 'January 2021',
-          //   onGoBack: () {},
-          //   onShowNext: () {},
-          // ),
-          ),
+      body: Home(),
     );
   }
 }
