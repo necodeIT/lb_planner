768e945a3ab32160ca7d96e1d7c7537239a1bcec
BuildTools <bmceachnie@student.tgm.ac.at>
2021-09-06 21:55:31 +0200

    Update main.dart

--- desktop/lib/main.dart
+++ desktop/lib/main.dart
@@ -1,8 +1,5 @@
 import 'dart:io';
 import 'package:desktop/home.dart';
-import 'package:desktop/widgets/views/calendar/calendar_switch.dart';
-import 'package:desktop/widgets/views/calendar/calendar_day.dart';
-import 'package:desktop/widgets/views/calendar/modules/modules_day.dart';
 import 'package:flutter/material.dart';
 import 'package:lb_planner/data.dart';
 import 'package:lb_planner/ui.dart';
@@ -19,8 +16,7 @@ void main() {
   runApp(app);
 }
 
-final app = MaterialApp(
-    home: App(), title: "LB Planner", scrollBehavior: NcScrollBehavior());
+final app = MaterialApp(home: App(), title: "LB Planner", scrollBehavior: NcScrollBehavior());
 
 class App extends StatefulWidget {
   App({Key? key}) : super(key: key);
@@ -32,8 +28,7 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    NcThemes.onCurrentThemeChange = () =>
-        setState(() => User.current.settings.theme = NcThemes.current.name);
+    NcThemes.onCurrentThemeChange = () => setState(() => User.current.settings.theme = NcThemes.current.name);
     return Scaffold(
       backgroundColor: NcThemes.current.secondaryColor,
       body: Home(),
