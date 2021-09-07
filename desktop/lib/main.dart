2cf64ffb7b6f18ac99b21badf5ae52e9d6216619
BuildTools <bmceachnie@student.tgm.ac.at>
2021-09-07 22:29:40 +0200

    *

--- desktop/lib/main.dart
+++ desktop/lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:catcher/catcher.dart';
 import 'package:desktop/home.dart';
 import 'package:flutter/material.dart';
 import 'package:lb_planner/data.dart';
@@ -9,14 +10,28 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
   NcThemes.current = NcThemes.all[User.current.settings.theme] ?? NcThemes.dark;
   // NcThemes.current = NcThemes.light;
+  // FlutterError.onError = (details) => errorNotifier.emit(onError, null, details);
   if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
     setWindowMinSize(const Size(1550.0, 850.0));
     setWindowMaxSize(Size.infinite);
   }
+  // runApp(app);
+
+  // CatcherOptions config = CatcherOptions(DialogReportMode(), [ConsoleHandler()]);
+
+  // Catcher(rootWidget: app, navigatorKey: GlobalKey<NavigatorState>());
+  // Catcher.getInstance().
   runApp(app);
 }
 
-final app = MaterialApp(home: App(), title: "LB Planner", scrollBehavior: NcScrollBehavior());
+final app = MaterialApp(
+  navigatorKey: Catcher.navigatorKey,
+  home: App(),
+  title: "LB Planner",
+  scrollBehavior: NcScrollBehavior(),
+);
+// final EventEmitter errorNotifier = EventEmitter();
+const onError = "onError";
 
 class App extends StatefulWidget {
   App({Key? key}) : super(key: key);
@@ -26,11 +41,43 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  // @override
+  // void initState() {
+  //   errorNotifier.on(
+  //     onError,
+  //     context,
+  //     (ev, catgirl) {
+  //       var error = ev.eventData as FlutterErrorDetails;
+
+  //       showDialog(
+  //         context: context,
+  //         builder: (context) => NcDialog(
+  //           title: "Something went wrong!",
+  //           body: NcBodyText(error.context.toString()),
+  //           onConfirm: () {
+  //             // TODO: send crash report
+  //             Navigator.of(context).pop();
+  //             setState(() {});
+  //           },
+  //           confirmText: "Send Report",
+  //         ),
+  //       );
+  //     },
+  //   );
+  //   super.initState();
+  // }
+
   @override
   Widget build(BuildContext context) {
     NcThemes.onCurrentThemeChange = () => setState(() => User.current.settings.theme = NcThemes.current.name);
+
     return Scaffold(
       backgroundColor: NcThemes.current.secondaryColor,
+      // body: NcButton(
+      //     text: "Crash",
+      //     onTap: () {
+      //       throw Exception("Deine mom ist fett");
+      //     },),
       body: Home(),
     );
   }
