b5a8bb8cd47128a115c07079e9a5ff404ca76e40
Blobii <66135041+Blobii@users.noreply.github.com>
2021-09-08 08:46:38 +0200

    calendar update

--- desktop/lib/widgets/views/settings/delete_profile.dart
+++ desktop/lib/widgets/views/settings/delete_profile.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:lb_planner/ui.dart';
 
 class DeleteProfile extends StatelessWidget {
-  const DeleteProfile({ Key? key }) : super(key: key);
+  const DeleteProfile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return 
+    return Container();
   }
 }
