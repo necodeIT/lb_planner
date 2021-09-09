41dc980e6c41cfea38f5cdc756a6f0c77caf1f86
Jszuecs <60018257+Jszuecs@users.noreply.github.com>
2021-09-09 20:25:25 +0200

    svgs

--- desktop/lib/widgets/views/admin/admin_panel.dart
+++ desktop/lib/widgets/views/admin/admin_panel.dart
@@ -1,9 +1,16 @@
+import 'package:desktop/widgets/views/admin/svg/database.dart';
+import 'package:desktop/widgets/views/admin/svg/feedback.dart';
+import 'package:desktop/widgets/views/admin/svg/stats.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:lb_planner/ui.dart';
 
 class AdminPanel extends StatefulWidget {
   const AdminPanel({Key? key}) : super(key: key);
 
+  static const double width = 400;
+  static const double height = 250;
+  static const double fontSize = 20;
   @override
   _AdminPanelState createState() => _AdminPanelState();
 }
@@ -11,7 +18,37 @@ class AdminPanel extends StatefulWidget {
 class _AdminPanelState extends State<AdminPanel> {
   @override
   Widget build(BuildContext context) {
-    return NcView(title: "Admin", content: NcGridView(children: []));
+    return NcView(
+      title: "Admin",
+      content: NcGridView(
+        children: [
+          NcContainer(
+            height: AdminPanel.height,
+            width: AdminPanel.width,
+            label: NcTitleText("Stats", fontSize: AdminPanel.fontSize),
+            leadingIcon: Icon(Icons.query_stats,
+                size: AdminPanel.fontSize, color: NcThemes.current.textColor),
+            body: NcVectorImage(code: stats_svg),
+          ),
+          NcContainer(
+            height: AdminPanel.height,
+            width: AdminPanel.width,
+            label: NcTitleText("Feedback", fontSize: AdminPanel.fontSize),
+            leadingIcon: Icon(Icons.chat_bubble,
+                size: AdminPanel.fontSize, color: NcThemes.current.textColor),
+            body: NcVectorImage(code: feedback_svg),
+          ),
+          NcContainer(
+            height: AdminPanel.height,
+            width: AdminPanel.width,
+            label: NcTitleText("Database", fontSize: AdminPanel.fontSize),
+            leadingIcon: Icon(FontAwesome5Solid.database,
+                size: AdminPanel.fontSize, color: NcThemes.current.textColor),
+            body: NcVectorImage(code: database_svg),
+          ),
+        ],
+      ),
+    );
     // return NcView(
     //   title: "Admin",
     //   content: NcGridView(
