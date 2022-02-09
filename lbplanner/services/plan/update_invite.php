f76fc0c967df9ce4af7f7fa44ce8cc80b2af368a
BuildTools <bmceachnie@student.tgm.ac.at>
2022-02-09 19:56:18 +0100

    implemented update invite
    hope i did not introduce some new bugs

--- lbplanner/services/plan/update_invite.php
+++ lbplanner/services/plan/update_invite.php
@@ -27,33 +27,37 @@ use local_lbplanner\helpers\notifications_helper;
 class plan_update_invite extends external_api {
     public static function update_invite_parameters() {
         return new external_function_parameters(array(
-            'planid' => new external_value(PARAM_INT, 'The id of the plan', VALUE_REQUIRED, null, NULL_NOT_ALLOWED),
-            'inviteeid' => new external_value(PARAM_INT, 'The id of the invited user', VALUE_REQUIRED, null, NULL_NOT_ALLOWED),
-            'status' => new external_value(PARAM_INT, 'The status of the invite', VALUE_REQUIRED, null, NULL_NOT_ALLOWED),
+        'planid' => new external_value(PARAM_INT, 'The id of the plan', VALUE_REQUIRED, null, NULL_NOT_ALLOWED),
+        'userid' => new external_value(PARAM_INT, 'The id of the invited user', VALUE_REQUIRED, null, NULL_NOT_ALLOWED),
+        'status' => new external_value(PARAM_INT, 'The status of the invite', VALUE_REQUIRED, null, NULL_NOT_ALLOWED),
         ));
     }
 
-    public static function update_invite($planid, $inviteeid, $status) {
+    public static function update_invite($planid, $userid, $status) {
         global $DB;
-        global $USER;
 
         self::validate_parameters(self::update_invite_parameters(), array(
-            'planid' => $planid,
-            'inviteeid' => $inviteeid,
-            'status' => $status,
+        'planid' => $planid,
+        'userid' => $userid,
+        'status' => $status,
         ));
 
-        if (!user_helper::check_access($inviteeid)) {
+        if (!user_helper::check_access($userid)) {
             throw new \moodle_exception('Access denied');
+        }
+
+        if ($status != plan_helper::INVITE_ACCEPTED && $status != plan_helper::INVITE_DECLINED) {
+            throw new \moodle_exception('Invalid status');
         }
 
         $invite = $DB->get_record(plan_helper::INVITES_TABLE,
         array(
             'planid' => $planid,
-            'inviteeid' => $inviteeid,
-            'status' => plan_helper::INVITE_PENDING),
-            '*',
-            MUST_EXIST
+            'userid' => $userid,
+            'status' => plan_helper::INVITE_PENDING
+        ),
+        '*',
+        MUST_EXIST
         );
 
         $invite->status = $status;
@@ -61,16 +65,52 @@ class plan_update_invite extends external_api {
         $DB->update_record(plan_helper::INVITES_TABLE, $invite);
 
         $trigger = $status == plan_helper::INVITE_ACCEPTED ?
-            notifications_helper::TRIGGER_INVITE_ACCEPTED
-            : notifications_helper::TRIGGER_INVITE_DECLINED;
+        notifications_helper::TRIGGER_INVITE_ACCEPTED
+        : notifications_helper::TRIGGER_INVITE_DECLINED;
 
         notifications_helper::notify_user($invite->inviterid, user_helper::get_complete_name($invite->invteeid) , $trigger);
 
-        // TODO: send notification to the inviter
         // TODO: Change plan access and delete old plan if inivite is accepted.
 
-        return array();
+        if ($status == plan_helper::INVITE_ACCEPTED) {
+            $oldplanid = plan_helper::get_plan_id($userid);
+
+            if (plan_helper::get_owner($oldplanid) == $userid) {
+
+                foreach (plan_helper::get_plan_members($oldplanid) as $member) {
+                    plan_leave_plan::leave_plan($member->userid, $oldplanid);
+                }
+
+                plan_clear_plan::clear_plan($userid, $oldplanid);
+
+                $DB->delete_records(plan_helper::TABLE, array('id' => $oldplanid));
+            }
+
+            $planaccess = $DB->get_record(
+                plan_helper::ACCESS_TABLE,
+                array(
+                    'planid' => $oldplanid,
+                    'userid' => $userid
+                ),
+                '*',
+                MUST_EXIST
+            );
+
+            $planaccess->accesstype = plan_helper::ACCESS_TYPE_READ;
+            $planaccess->planid = $planid;
+
+            $DB->update_record(plan_helper::ACCESS_TABLE, $planaccess);
+        }
+
+        return array(
+            'inviterid' => $invite->inviterid,
+            'inviteeid' => $invite->inviteeid,
+            'planid' => $invite->planid,
+            'status' => $invite->status,
+            'timestamp' => $invite->timestamp,
+        );
     }
+
 
     public static function update_invite_returns() {
         return new external_single_structure(
