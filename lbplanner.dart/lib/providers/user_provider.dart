e1581c08fc81acef2cebe0a93e930b1d72cdd8a8
bmceachnie <bmceachnie@student.tgm.ac.at>
2022-03-30 18:08:18 +0200

    UserProvider docs

--- lbplanner.dart/lib/providers/user_provider.dart
+++ lbplanner.dart/lib/providers/user_provider.dart
@@ -1,11 +1,16 @@
 part of lbplanner_api;
 
 /// Provides the current user
+final userProvider = Provider((ref) => UserProvider());
+
+/// Provides the current user
 class UserProvider extends StateNotifier<User> {
   /// Provides the current user
   UserProvider() : super(User.empty());
 
-  /// Performs a login request with the given [username] and [password]
+  /// Performs a login request with the given [username] and [password].
+  /// If the user is successfully logged in, the [User] is updated.
+  /// If the user was not registered, the user will be automatically registered.
   Future<RawApiResponse> login(String username, String password, {required Languages language, required String theme}) async {
     var lpa = await UserApi.login(username, password);
     var moodleMobileApp = await Api.requestToken(password, username, ApiServices.moodle_mobile_app);
