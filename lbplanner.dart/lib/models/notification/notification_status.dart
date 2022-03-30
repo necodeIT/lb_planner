30148a18c83facc9e09c19059657bcc463e3102c
bmceachnie <bmceachnie@student.tgm.ac.at>
2022-03-30 19:31:13 +0200

    dunno

---
+++ lbplanner.dart/lib/models/notification/notification_status.dart
@@ -0,0 +1,10 @@
+part of lbplanner_api;
+
+/// Status of the notification.
+enum NotificationStatus {
+  /// Notification is unread.
+  unread,
+
+  /// Notification is read.
+  read,
+}
---
+++ lbplanner.dart/lib/models/user/access_levels.dart
@@ -0,0 +1,19 @@
+part of lbplanner_api;
+
+/// Contains any possible roles a user can have
+enum AccessLevels {
+  /// Admin role
+  admin,
+
+  /// Moderator role
+  moderator,
+
+  /// Teacher role
+  teacher,
+
+  /// Student role
+  student,
+
+  /// Restricted information available
+  restricted
+}
