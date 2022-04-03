27efc51799384c32e69730acb0664fb3f9602bcb
bmceachnie <60017181+bmceachnie@users.noreply.github.com>
2022-04-03 18:48:20 +0200

    Update invite_user.php

--- lbplanner/services/plan/invite_user.php
+++ lbplanner/services/plan/invite_user.php
@@ -106,7 +106,7 @@ class plan_invite_user extends external_api {
         // Notifiy the invitee that he/she/it/they/xier/* has been invited.
         notifications_helper::notify_user(
             $inviteeid,
-            json_encode(array("inviteer"=>invierid)),
+            json_encode(array("inviterid"=>$invierid)),
             notifications_helper::TRIGGER_INVITE
         );
 
