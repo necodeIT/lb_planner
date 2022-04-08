56a80c500a530e8c0a5ce5f97795200881a26fe3
jszuecs-tgm <jszuecs@student.tgm.ac.at>
2022-04-08 18:05:58 +0200

    dropBox

--- lb_planner/lib/widgets.dart
+++ lb_planner/lib/widgets.dart
@@ -1,5 +1,6 @@
 library lbplanner_widgets;
 
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:nekolib_ui/core.dart';
 import 'package:nekolib_ui/utils.dart';
@@ -7,6 +8,7 @@ import 'package:nekolib_ui/utils.dart';
 part 'widgets/button.dart';
 part 'widgets/textbutton.dart';
 part "widgets/checkbox.dart";
+part 'widgets/dropdown.dart';
 
 /// Radius for all rounded rectangles.
 const double kRadius = 5;
---
+++ lb_planner/lib/widgets/dropdown.dart
@@ -0,0 +1,34 @@
+part of lbplanner_widgets;
+
+/// Themed [DropdownButton] widget.
+class NcDropdown<T> extends StatelessWidget {
+  /// Themed [DropdownButton] widget.
+  const NcDropdown({Key? key, required this.items, required this.onChanged, this.color, this.value}) : super(key: key);
+
+  /// The items to display in the dropdown.
+  final List<DropdownMenuItem<T>> items;
+
+  /// The callback that is called when the user selects an item.
+  final void Function(T?) onChanged;
+
+  /// The color of the dropdown button.
+  final Color? color;
+
+  /// The value of the dropdown button.
+  final T? value;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<T>(
+      value: value,
+      items: items,
+      onChanged: onChanged,
+      dropdownColor: color ?? primaryColor,
+      style: TextStyle(color: textColor),
+      icon: Icon(FluentIcons.chevron_down_12_filled, color: textColor),
+      borderRadius: BorderRadius.circular(kRadius),
+      underline: SizedBox.shrink(),
+      focusColor: Colors.transparent,
+    );
+  }
+}
