9af76218d9ef2c9eb400a93280a945da5d145a47
jszuecs-tgm <jszuecs@student.tgm.ac.at>
2022-04-08 16:55:44 +0200

    Textbutton

--- lb_planner/lib/widgets/textbutton.dart
+++ lb_planner/lib/widgets/textbutton.dart
@@ -1,17 +1,37 @@
 part of lbplanner_widgets;
 
+/// The [NcTextButton] widget is a button that displays clickable text and an icon.
 class NcTextButton extends StatelessWidget {
-  const NcTextButton({ Key? key }) : super(key: key);
+  /// The [NcTextButton] widget is a button that displays clickable text and an icon.
+  NcTextButton({Key? key, required this.text, this.onPressed, this.leadingIcon, this.trailingIcon}) : super(key: key) {
+    assert(leadingIcon != null || trailingIcon != null);
+  }
 
+  /// The text the button displays.
   final String text;
+
+  /// The function to call when the button is pressed.
   final VoidCallback? onPressed;
+
+  /// The icon to display on the leading side of the button.
   final IconData? leadingIcon;
+
+  /// The icon to display on the trailing side of the button.
   final IconData? trailingIcon;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return TextButton(
+      onPressed: onPressed,
+      child: Row(
+        children: [
+          if (leadingIcon != null) Icon(leadingIcon, color: textColor),
+          if (leadingIcon != null) NcSpacing.small(),
+          NcCaptionText(text),
+          if (trailingIcon != null) NcSpacing.small(),
+          if (trailingIcon != null) Icon(trailingIcon, color: textColor),
+        ],
+      ),
     );
   }
 }
