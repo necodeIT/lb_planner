f30a07769984677937ed03e471d8885274973fdd
bmceachnie <bmceachnie@student.tgm.ac.at>
2022-04-08 17:30:34 +0200

    icons

--- lb_planner/lib/widgets.dart
+++ lb_planner/lib/widgets.dart
@@ -7,7 +7,6 @@ import 'package:nekolib_ui/utils.dart';
 part 'widgets/button.dart';
 part 'widgets/textbutton.dart';
 part "widgets/checkbox.dart";
-part "widgets/icons.dart";
 
 /// Radius for all rounded rectangles.
 const double kRadius = 5;
