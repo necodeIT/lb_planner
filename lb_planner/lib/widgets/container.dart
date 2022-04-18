e261b4a303476f0959dc4e5ff462fcabc35da4be
jszuecs-tgm <jszuecs@student.tgm.ac.at>
2022-04-19 00:25:24 +0200

    unfinished Container

--- lb_planner/lib/widgets/container.dart
+++ lb_planner/lib/widgets/container.dart
@@ -1,13 +1,37 @@
 part of lbplanner_widgets;
 
+/// Themed [Container] widget.
 class LpContainer extends StatelessWidget {
-  const LpContainer({Key? key, required this.title, this.icon, this.menue, required this.body, this.width, this.height}) : super(key: key);
+  /// Themed [Container] widget.
+  LpContainer({Key? key, required this.title, this.leadingIcon, this.trailingIcon, required this.body, this.width, this.height}) : super(key: key) {
+    window = false;
+  }
+
+  /// Themed WindowContainer widget.
+  LpContainer.window({Key? key, required this.title, this.leadingIcon, this.trailingIcon, required this.body, this.width, this.height}) : super(key: key) {
+    window = true;
+  }
+
+  /// The title of the container.
   final String title;
-  final Widget? icon;
-  final Widget? menue;
+
+  /// The leading icon of the container.
+  final Widget? leadingIcon;
+
+  /// The trailing icon of the container.
+  final Widget? trailingIcon;
+
+  /// The body of the container.
   final Widget body;
+
+  /// The width of the container.
   final double? width;
+
+  /// The height of the container.
   final double? height;
+
+  /// If true, the container is a window.
+  late final bool window;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -15,15 +39,30 @@ class LpContainer extends StatelessWidget {
       height: height,
       child: Column(
         children: [
-          Row(
-            children: [
-              if (icon != null) icon ?? Container(),
-              if (icon != null) NcSpacing.small(),
-              Expanded(child: NcTitleText(title)),
-              if (menue != null) NcSpacing.small(),
-              if (menue != null) menue ?? Container(),
-            ],
+          ConditionalWrapper(
+            condition: window,
+            wrapper: (context, child) => Container(
+              decoration: BoxDecoration(
+                color: secondaryColor,
+                borderRadius: BorderRadius.vertical(top: Radius.circular(kRadius)),
+              ),
+              child: child,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    if (leadingIcon != null) leadingIcon!,
+                    if (leadingIcon != null) NcSpacing.small(),
+                    NcTitleText(title),
+                  ],
+                ),
+                if (trailingIcon != null) trailingIcon!,
+              ],
+            ),
           ),
+          body,
         ],
       ),
     );
