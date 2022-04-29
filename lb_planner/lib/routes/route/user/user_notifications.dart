ec54c22f068dec4462a1f1a9490e99e1a38b3207
bmceachnie <bmceachnie@student.tgm.ac.at>
2022-04-29 16:49:09 +0200

    Update user_notifications.dart

--- lb_planner/lib/routes/route/user/user_notifications.dart
+++ lb_planner/lib/routes/route/user/user_notifications.dart
@@ -1,19 +1,40 @@
 part of lbplanner_routes;
 
 /// Popup that displays all notifications the user received.
-class UserNotifications extends StatelessWidget {
+class UserNotifications extends StatefulWidget {
   /// Popup that displays all notifications the user received.
   const UserNotifications({Key? key}) : super(key: key);
+
+  @override
+  State<UserNotifications> createState() => _UserNotificationsState();
+}
+
+class _UserNotificationsState extends State<UserNotifications> {
+  bool _isShowing = false;
+
+  void _onShow(NotificationsProvider notificationsProvider) {
+    setState(() {
+      _isShowing = true;
+    });
+
+    notificationsProvider.markAllAsRead();
+  }
+
+  void _onHide() {
+    setState(() {
+      _isShowing = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Consumer(
       builder: (context, ref, _) {
-        var notifications = ref.watch(notificationsProvider);
-        var controller = ref.read(notificationsController);
+        var notifications = _isShowing ? ref.read(notificationsProvider) : ref.watch(notificationsProvider);
 
         var child = LpPopup(
-          onShow: () => controller.markAllAsRead(),
+          onShow: () => _onShow(ref.read(notificationsController)),
+          onHide: _onHide,
           child: LpIcon(Icons.notifications_outlined),
           popupBuilder: UserNotificationsPopup.builder,
           offset: const Offset(-10, 0),
