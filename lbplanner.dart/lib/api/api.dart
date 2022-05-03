0adfce08385b8e23a9b4eb2da054634ed193a628
bmceachnie <60017181+bmceachnie@users.noreply.github.com>
2022-05-03 22:52:04 +0200

    Update api.dart

--- lbplanner.dart/lib/api/api.dart
+++ lbplanner.dart/lib/api/api.dart
@@ -66,7 +66,7 @@ class Api {
   }
 
   static void _logResponse(RawApiResponse response) {
-    log("Response: ${response.response.statusCode}${response.failed ? ", Message: '${response.errorMessage}'" : ""}", response.succeeded ? LogTypes.success : LogTypes.error, 2);
+    log("Response: ${response.response.statusCode}${response.failed ? ", Message: '${response.errorMessage}'" : ""}", response.succeeded ? LogTypes.success : LogTypes.error, 1);
   }
 
   /// Callback called when an API call reults in an error.
