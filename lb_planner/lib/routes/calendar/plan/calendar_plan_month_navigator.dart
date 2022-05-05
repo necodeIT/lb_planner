0df47b32fec6cde60107166d67cd0433808a0b5b
bmceachnie <bmceachnie@student.tgm.ac.at>
2022-05-05 21:17:03 +0200

    Create calendar_plan_month_navigator.dart

---
+++ lb_planner/lib/routes/calendar/plan/calendar_plan_month_navigator.dart
@@ -0,0 +1,37 @@
+part of lbplanner_routes;
+
+/// Navigation bar for [CalendarPlanMonth].
+class CalendarPlanMonthNavigator extends StatelessWidget {
+  /// Navigation bar for [CalendarPlanMonth].
+  const CalendarPlanMonthNavigator({Key? key, required this.onNextMonth, required this.onPreviousMonth, required this.currentMonth}) : super(key: key);
+
+  /// Callback for the next month button.
+  final VoidCallback onNextMonth;
+
+  /// Callback for the previous month button.
+  final VoidCallback onPreviousMonth;
+
+  /// The current month to display.
+  final DateTime currentMonth;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        GestureDetector(
+          onTap: onPreviousMonth,
+          child: LpIcon(Icons.chevron_left),
+        ),
+        Center(
+          child: NcTitleText(
+            DateFormat("MMMM yyyy").format(currentMonth),
+          ),
+        ),
+        GestureDetector(
+          onTap: onNextMonth,
+          child: LpIcon(Icons.chevron_right),
+        ),
+      ],
+    );
+  }
+}
