5c160a7efd191e39083d08a5768ad6214f64dcc7
bmceachnie <bmceachnie@student.tgm.ac.at>
2022-05-08 13:02:28 +0200

    Update feedback.dart

--- lb_planner/lib/routes/settings/feedback/feedback.dart
+++ lb_planner/lib/routes/settings/feedback/feedback.dart
@@ -60,61 +60,60 @@ class _SettingsFeedbackState extends State<SettingsFeedback> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer(builder: (context, ref, _) {
-      var feedback = ref.watch(feedbackController);
-
-      return LpContainer(
-        title: t.settings_feedback,
-        child: ConditionalWrapper(
-          condition: _submitFuture != null,
-          wrapper: (context, child) => FutureBuilder(
-            future: _submitFuture,
-            builder: (context, snapshot) => ConditionalWidget(
-              condition: snapshot.connectionState.isDone,
-              trueWidget: (context) => child,
-              falseWidget: (context) => LpLoadingIndicator.circular(),
-            ),
-          ),
-          child: Column(
-            children: [
-              Align(
-                alignment: Alignment.centerLeft,
-                child: LpDropdown<FeedbackTypes>(
-                  fontSize: SettingsFeedback.fontSize,
-                  value: _feedbackType,
-                  items: [
-                    DropdownMenuItem(child: NcBodyText(t.settings_feedback_types_bug), value: FeedbackTypes.bug),
-                    DropdownMenuItem(child: NcBodyText(t.settings_feedback_types_error), value: FeedbackTypes.error),
-                    DropdownMenuItem(child: NcBodyText(t.settings_feedback_types_suggestion), value: FeedbackTypes.suggestion),
-                    DropdownMenuItem(child: NcBodyText(t.settings_feedback_types_other), value: FeedbackTypes.other),
-                  ],
-                  onChanged: _setFeedbackType,
-                ),
-              ),
-              NcSpacing.xs(),
-              Expanded(
-                child: LpTextField.filled(
-                  fontSize: SettingsFeedback.fontSize,
-                  controller: _feedbackController,
-                  placeholder: t.settings_feedback_placeholder,
-                  multiline: true,
-                ),
-              ),
-              NcSpacing.medium(),
-              Align(
-                alignment: Alignment.centerRight,
-                child: LpTextButton(
-                  text: t.settings_feedback_submit,
-                  trailingIcon: Feather.arrow_right_circle,
-                  onPressed: () => _submitFeedback(feedback, context),
-                  fontSize: SettingsFeedback.fontSize,
-                ),
-              ),
-              NcSpacing.xs(),
-            ],
+    return LpContainer(
+      title: t.settings_feedback,
+      child: ConditionalWrapper(
+        condition: _submitFuture != null,
+        wrapper: (context, child) => FutureBuilder(
+          future: _submitFuture,
+          builder: (context, snapshot) => ConditionalWidget(
+            condition: snapshot.connectionState.isDone,
+            trueWidget: (context) => child,
+            falseWidget: (context) => LpLoadingIndicator.circular(),
           ),
         ),
-      );
-    });
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: LpDropdown<FeedbackTypes>(
+                fontSize: SettingsFeedback.fontSize,
+                value: _feedbackType,
+                items: [
+                  DropdownMenuItem(child: NcBodyText(t.settings_feedback_types_bug), value: FeedbackTypes.bug),
+                  DropdownMenuItem(child: NcBodyText(t.settings_feedback_types_error), value: FeedbackTypes.error),
+                  DropdownMenuItem(child: NcBodyText(t.settings_feedback_types_suggestion), value: FeedbackTypes.suggestion),
+                  DropdownMenuItem(child: NcBodyText(t.settings_feedback_types_other), value: FeedbackTypes.other),
+                ],
+                onChanged: _setFeedbackType,
+              ),
+            ),
+            NcSpacing.xs(),
+            Expanded(
+              child: LpTextField.filled(
+                fontSize: SettingsFeedback.fontSize,
+                controller: _feedbackController,
+                placeholder: t.settings_feedback_placeholder,
+                multiline: true,
+              ),
+            ),
+            NcSpacing.medium(),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Consumer(
+                builder: (_, ref, __) => LpTextButton(
+                  text: t.settings_feedback_submit,
+                  trailingIcon: Feather.arrow_right_circle,
+                  // Don't ask why but i get exceptions when using the context provided from the builder.
+                  onPressed: () => _submitFeedback(ref.watch(feedbackController), context),
+                  fontSize: SettingsFeedback.fontSize,
+                ),
+              ),
+            ),
+            NcSpacing.xs(),
+          ],
+        ),
+      ),
+    );
   }
 }
