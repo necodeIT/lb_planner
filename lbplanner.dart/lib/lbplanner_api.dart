6572d33126c9231618d562a08047dbcc74f4f75a
bmceachnie <60017181+bmceachnie@users.noreply.github.com>
2022-05-24 20:13:28 +0200

    Update lbplanner_api.dart

--- lbplanner.dart/lib/lbplanner_api.dart
+++ lbplanner.dart/lib/lbplanner_api.dart
@@ -91,4 +91,4 @@ part 'disk/user_disk.dart';
 part 'disk/disk.dart';
 
 /// The rate with which the api refreshes underlying data
-const kApiRefreshRate = Duration(seconds: 30);
+const kApiRefreshRate = Duration(seconds: 10);
