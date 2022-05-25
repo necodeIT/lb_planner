14f2b5a07cae957d396c924f61e9f11b66d29b86
bmceachnie <60017181+bmceachnie@users.noreply.github.com>
2022-05-25 14:09:45 +0200

    Update user_utils.dart

--- lbplanner.dart/lib/utils/user_utils.dart
+++ lbplanner.dart/lib/utils/user_utils.dart
@@ -3,7 +3,7 @@ part of lbplanner_api;
 /// Utils for [User]
 extension UserUtils on User {
   /// Whether the user can make requests.
-  bool get canMakeRequests => !restricted && !loading;
+  bool get canMakeRequests => !restricted && !loading && await Connectivity().checkConnectivity() != ConnectivityResult.none;
 
   /// Whether the user has an access level of at least [UserAccessLevels.moderator].
   bool get isElevated => accessLevel.isAdmin || accessLevel.isModerator;
