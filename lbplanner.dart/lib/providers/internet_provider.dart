fefe10714b149a7f571dad6606577cfe8958930e
bmceachnie <60017181+bmceachnie@users.noreply.github.com>
2022-05-25 11:55:38 +0200

    Update internet_provider.dart

--- lbplanner.dart/lib/providers/internet_provider.dart
+++ lbplanner.dart/lib/providers/internet_provider.dart
@@ -1,9 +1,15 @@
 part of lbplanner_api;
 
 /// Provides the current internet connection.
+final internetProvider = StateNotifierProvider<InternetProvider, bool>((_) => InternetProvider());
+
+/// Controller for the [internetProvider].
+final internetController = internetProvider.notifier;
+
+/// Provides the current internet connection.
 class InternetProvider extends StateNotifier<bool> {
   /// Provides the current internet connection.
-  InternetProvider(bool value) : super(false);
+  InternetProvider() : super(true);
 
   void _update(ConnectivityResult result) {
     setState(result != ConnectivityResult.none);
