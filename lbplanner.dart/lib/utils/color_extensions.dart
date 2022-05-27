1c19a2cade23456234b0d456b1f8d28be03c8e10
bmceachnie <60017181+bmceachnie@users.noreply.github.com>
2022-05-27 13:23:27 +0200

    Update color_extensions.dart

--- lbplanner.dart/lib/utils/color_extensions.dart
+++ lbplanner.dart/lib/utils/color_extensions.dart
@@ -3,8 +3,8 @@ part of lbplanner_api;
 /// Provides helper methods for the [Color] type.
 extension ColorUtils on Color {
   /// Returns the hexadecimal representation of the color.
-  String get hexCode => "#${value.toRadixString(16)}";
+  String get hexCode => "#${value.toRadixString(16).replaceAll(r"ff", r"")}";
 
   /// Color from hexadecimal representation.
-  static Color fromHexCode(String hexCode) => Color(int.parse(hexCode.replaceAll("#", ""), radix: 16));
+  static Color fromHexCode(String hexCode) => Color(int.parse(hexCode.toLowerCase().replaceAll("#ff", ""), radix: 16));
 }
