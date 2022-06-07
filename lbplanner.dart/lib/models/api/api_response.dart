6b4da9dee5ed98dff17bef85012b85b6f644b469
bmceachnie <60017181+bmceachnie@users.noreply.github.com>
2022-06-07 13:18:10 +0200

    Update api_response.dart

--- lbplanner.dart/lib/models/api/api_response.dart
+++ lbplanner.dart/lib/models/api/api_response.dart
@@ -23,7 +23,7 @@ class RawApiResponse {
   /// Wrapper for the given [response].
   /// Provides utilities to handle errors and parse the response body.
   RawApiResponse(this.response) {
-    var payload = jsonDecode(Uri.decodeComponent(response.body.replaceAll('%0A','\\n').replaceAll('%22','\"')));
+    var payload = jsonDecode(Uri.decodeComponent(response.body.replaceAll('%0A','\\n').replaceAll('%22',r'\"').replaceAll("'",r"\'")));
 
     isList = payload.runtimeType == List;
 
