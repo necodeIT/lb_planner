87ebcbfd395776913145ca126cb4406c49817a86
bmceachnie <60017181+bmceachnie@users.noreply.github.com>
2022-08-31 23:28:05 +0200

    Update plugin_config_api.dart

--- lib/api/plugin_config_api.dart
+++ lib/api/plugin_config_api.dart
@@ -3,7 +3,7 @@ part of lbplanner_engine;
 /// Provides api methods for plugin config functions.
 class PluginConfigApi {
   /// Retrieves the [release version](https://github.com/necodeIT/lb_planner/blob/main/versioning.md#:~:text=be%20the%20same.-,Backend%20versioning,-The%20backend%20has) of the moodle plugin.
-  Future<ApiResponse<Version>> getVersion(String token) async {
+  static Future<ApiResponse<Version>> getVersion(String token) async {
     var response = await Api.makeRequest(
       functionName: "local_lbplanner_config_get_version",
       token: token,
