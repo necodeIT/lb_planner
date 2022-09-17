886a32cd38fab5969e4930ae8b8b54c84a056dd2
RiedleroD <riedler@gelse.eu>
2022-09-17 20:42:31 +0200

    merged og1 and page1

--- index.php
+++ index.php
@@ -8,33 +8,27 @@
 			include('./snippets/navbar.php');
 			genNavbar(1);
 		?>
-		<section class="screenpage" id="page1">
-			<div id="og1">
-				<h1>Lorem ipsum dolor sit amet, consectetur</h2>
-				<h3>Lorem ipsum dolor sit amet, consectetur</h3>
-				<span>
-					<a class="btn" href="https://soundcloud.com/riedler-musics/cupheads-title-screen-8-bit">😛 DOWNLOAD</a>
-					<?php
-						genExtRef('Docs','/docs/');
-						genExtRef('Admin','/docs/#admin');
-					?>
-				</span>
-				<img class="card" src="/resources/example_cal.png" alt="example screenshot"/>
-			</div>
+		<section class="screenpage" id="og1">
+			<h1>Lorem ipsum dolor sit amet, consectetur</h2>
+			<h3>Lorem ipsum dolor sit amet, consectetur</h3>
+			<span>
+				<a class="btn" href="https://soundcloud.com/riedler-musics/cupheads-title-screen-8-bit">😛 DOWNLOAD</a>
+				<?php
+					genExtRef('Docs','/docs/');
+					genExtRef('Admin','/docs/#admin');
+				?>
+			</span>
+			<img class="card" src="/resources/example_cal.png" alt="example screenshot"/>
 			<h1 class="sectionheader">Fit your style</h1>
 			<div id="themebox">
-				<a class="card" href="./?theme=light">
-					LIGHT
-				</a>
-				<a class="card" href="./?theme=dark">
-					DARK
-				</a>
-				<a class="card" href="./?theme=ocean">
-					OCEAN
-				</a>
-				<a class="card" href="./?theme=sakura">
-					SAKURA
-				</a>
+				<div>
+					<a class="card" href="./?theme=light">LIGHT</a>
+					<a class="card" href="./?theme=dark">DARK</a>
+				</div>
+				<div>
+					<a class="card" href="./?theme=ocean">OCEAN</a>
+					<a class="card" href="./?theme=sakura">SAKURA</a>
+				</div>
 			</div>
 			<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</span>
 		</section>
