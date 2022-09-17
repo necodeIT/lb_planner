e65f2c778c5418c2f2ad3a37496fdf0628478f90
RiedleroD <riedler@gelse.eu>
2022-09-17 22:42:13 +0200

    added second and third sections to download page

--- index.php
+++ index.php
@@ -33,8 +33,33 @@
 			</div>
 			<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</span>
 		</section>
-		<section class="screenpage">
-			b
+		<section class="screenpage" id="pg2">
+			<img class="card" src="/resources/example_cal.png" alt="example screenshot"/>
+			<h2>Lorem ipsum dolor sit amet,</h2>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+				do eiusmod tempor incididunt ut labore et dolore magna
+				aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+				ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+				cupidatat non proident, sunt in culpa qui officia deserunt
+				mollit anim id est laborum.
+			</p>
+		</section>
+		<section class="screenpage" id="pg3">
+			<img class="card" src="/resources/example_cal.png" alt="example screenshot"/>
+			<h2>Lorem ipsum dolor sit amet,</h2>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+				do eiusmod tempor incididunt ut labore et dolore magna
+				aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+				ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+				cupidatat non proident, sunt in culpa qui officia deserunt
+				mollit anim id est laborum.
+			</p>
 		</section>
 	</body>
 </html>
