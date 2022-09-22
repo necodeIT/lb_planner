3910596c0c207facd201675d3433332fa575e3e7
RiedleroD <riedler@gelse.eu>
2022-09-22 14:47:53 +0200

    added another section of type 2

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
 			</div>
 			<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</span>
 		</section>
-		<section class="screenpage" id="pg2">
+		<section class="screenpage pagetype2">
 			<img class="card" src="/resources/example_cal.png" alt="example screenshot"/>
 			<h2>Lorem ipsum dolor sit amet,</h2>
 			<p>
@@ -54,7 +54,21 @@
 				mollit anim id est laborum.
 			</p>
 		</section>
-		<section class="screenpage" id="pg3">
+		<section class="screenpage pagetype2">
+			<img class="card" src="/resources/example_cal.png" alt="example screenshot"/>
+			<h2>Lorem ipsum dolor sit amet,</h2>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+				do eiusmod tempor incididunt ut labore et dolore magna
+				aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+				ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+				cupidatat non proident, sunt in culpa qui officia deserunt
+				mollit anim id est laborum.
+			</p>
+		</section>
+		<section class="screenpage pagetype2">
 			<img class="card" src="/resources/example_cal.png" alt="example screenshot"/>
 			<h2>Lorem ipsum dolor sit amet,</h2>
 			<p>
