9f2aa91b5a9639d8082e88bdd1166000af575b6b
RiedleroD <riedler@gelse.eu>
2022-10-09 15:22:48 +0200

    draft of strings for subpage docs

--- locale/reload.sh
+++ locale/reload.sh
@@ -3,11 +3,28 @@
 # needs FILE permission for mysql user
 
 langs=('en' 'de')
+transDBs=('Translation' 'DocsTitles' 'DocsHeadings' 'DocsTexts')
+filepaths=('' 'docs_titles_' 'docs_headings_' 'docs_texts_')
 
-#IGNORE because otherwise you could accidentally change string keys & it throws an foreign key constraint error I'd have to explicitly ignore
-mysql lbpages -u lbplanner -e "LOAD DATA INFILE \"$(realpath ./keys.txt)\" IGNORE INTO TABLE TranslationKeys FIELDS TERMINATED BY ','"
+#IGNORE because otherwise you could accidentally change string keys
+query="LOAD DATA INFILE \"$(realpath ./keys.txt)\" IGNORE INTO TABLE TranslationKeys FIELDS TERMINATED BY ',';";
+echo "$query"
+mysql lbpages -u lbplanner -e "$query"
 
 for ((i=0;i<"${#langs[@]}";i++));
 do :
-	mysql lbpages -u lbplanner -e "LOAD DATA INFILE \"$(realpath ./${langs[$i]}.txt)\" REPLACE INTO TABLE Translation_${langs[$i]} FIELDS TERMINATED BY ',' ESCAPED BY '\\\\'"
+	#clearing tables in reverse order as to not run into foreign key constraints
+	for ((j="${#transDBs[@]}"-1;j>-1;j--));
+	do :
+		query="DELETE FROM ${transDBs[$j]}_${langs[$i]};"
+		echo "$query"
+		mysql lbpages -u lbplanner -e "$query"
+	done
+	#reloading tables
+	for ((j=0;j<"${#transDBs[@]}";j++));
+	do :
+		query="LOAD DATA INFILE \"$(realpath ./${filepaths[$j]}${langs[$i]}.txt)\" REPLACE INTO TABLE ${transDBs[$j]}_${langs[$i]} FIELDS TERMINATED BY ',' ESCAPED BY '\\\\';"
+		echo "$query"
+		mysql lbpages -u lbplanner -e "$query"
+	done
 done
