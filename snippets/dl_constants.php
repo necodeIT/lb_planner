b922c273700583d7cfa8320ee77009dde31fdb7c
bmceachnie <60017181+bmceachnie@users.noreply.github.com>
2022-10-20 12:24:42 +0200

    Create dl_constants.php

---
+++ snippets/dl_constants.php
@@ -0,0 +1,17 @@
+<?php
+# AUTOMATICALLY MAINTAINED BY GITHUB ACTIONS — DO NOT EDIT
+
+/**
+ * Linux download url.
+ */
+const dl_linux = "";
+
+/**
+ * Windows download url.
+ */
+const dl_windows = "";
+
+/**
+ * Plugin download url.
+ */
+const dl_plugin = "";
