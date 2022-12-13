9fb6e3f12160ec52b5188394016310ef989e9664
mkocagoel <mkocagoel@student.tgm.ac.at>
2022-12-13 16:47:55 +0100

    adding version number

--- lbplanner/version.php
+++ lbplanner/version.php
@@ -20,6 +20,6 @@ $release = '0.0.0';
 
 $plugin->component = 'local_lbplanner';
 $plugin->release = 'Alpha v.'.$release;
-$plugin->version = 20221213040;
+$plugin->version = 20221213041;
 
 set_config('release', $release, 'local_lbplanner');
