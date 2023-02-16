b5121ea4cfca2745e0630d9e6a537d957ecc6b1d
RiedleroD <github@riedler.wien>
2023-02-16 19:52:03 +0100

    stylesheets now get inserted as link tags instead of CSS @import statements

--- snippets/head.php
+++ snippets/head.php
@@ -1,19 +1,16 @@
 <!DOCTYPE html>
+
 <html lang="en">
 	<head>
 		<meta charset="UTF-8"/>
 		<title>LB Planner</title>
 		<link rel="icon" type="image/svg" href="/resources/logo.svg"/>
-		<style>
-			<?php
-				if(isset($stylesheets)){
-					foreach($stylesheets as $stylesheet){
-						echo "@import '/style/$stylesheet.css';";
-					}
-				}
-			?>
-		</style>
 		<?php
+			if(isset($stylesheets)){
+				foreach($stylesheets as $stylesheet){
+					echo "<link rel='stylesheet' href='/style/$stylesheet.css'>";
+				}
+			}
 			if(isset($scripts)){
 				foreach($scripts as $script){
 					echo "<script async src='/js/$script.js'></script>";
