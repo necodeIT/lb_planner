c58c0fb9621aa210131f40f3de86276c30f0aed3
RiedleroD <github@riedler.wien>
2023-02-16 16:25:11 +0100

    added download url for mac

--- snippets/dl_constants.php
+++ snippets/dl_constants.php
@@ -12,6 +12,11 @@ const dl_linux = "";
 const dl_windows = "";
 
 /**
+ * MacOS download url.
+ */
+const dl_mac = "";
+
+/**
  * Plugin download url [@p0.0.2](https://github.com/necodeIT/lb_planner/releases/tag/p0.0.2).
  */
 const dl_plugin = "https://github.com/necodeIT/lb_planner/releases/download/p0.0.2/lb_planner.zip";
