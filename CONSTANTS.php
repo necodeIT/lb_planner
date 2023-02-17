c7c7b454054271c01b16d27fa3cde61f730870f8
RiedleroD <github@riedler.wien>
2023-02-17 14:02:44 +0100

    added constants file for easier environment migration

---
+++ CONSTANTS.php
@@ -0,0 +1,9 @@
+<?php
+
+const supported_languages = array('en','de');
+const supported_systems = array('linux','mac','windows');
+
+// enter your url root here
+// if your base url is e.g. "https://projekte.tgm.ac.at/lb-planner/", then
+// this should be "/lb-planner"
+const urlroot = "";
