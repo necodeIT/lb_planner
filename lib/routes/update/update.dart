efdaceddf20c93b6330d3b3cbff346bf800dc146
bmceachnie <60017181+bmceachnie@users.noreply.github.com>
2023-02-24 18:37:36 +0100

    removed hardcoded patch notes

--- lib/routes/update/update.dart
+++ lib/routes/update/update.dart
@@ -58,64 +58,6 @@ class _UpdateRouteState extends State<UpdateRoute> {
           Catcher.reportCheckedError(kUpdateErrorKeyword + update.error, StackTrace.current);
         }
 
-        // ! Testing only
-        update = update.copyWith(updateAvailable: true, patchNotes: '''
-# Tollere bacis
-
-![Screenshot](https://projekte.tgm.ac.at/lb-planner/resources/example_cal.png)
-
-## Vacuaque pulsant summa exploratis reppulit
-
-Lorem markdownum esse maledictaque mentem bis iugulum et factum digiti iuvenale
-agitur non mando! Fretum nec derecti frondes; gemitus quem, tollens sub iam
-diriguit. Induta thalamos resistere anilem neque Tethys **es deos**: velamina
-Phoebus dumque novercam. Castra praeterit bellum mihi diro igneus, Thaumantidos
-crinem, fulvis thalamique.
-
-- Fugit inludens nulla fugato caecae sanguine saepe
-- Credere omni patulas avis exsurgo riguo spretis
-- Vincta ullis quoque turba
-
-Molli petit nuda eiaculatur in undas! Timori suis Centauros abductas videri. Nec
-his aut esse Rhodopeius, vidit!
-
-> Iovem ab posito et quantum campo nomina relinquent et quem *obiectat quid*
-> boum circum. Annua [repetet artus](http://nisi.org/anima.html) praecordia ut
-> fuit [Cnosiaci](http://tuncdempto.com/) sepulcro est cadit, et beatum sustinet
-> prolis inducta ora idem minus nulla.
-
-## Arma securae florente fossas
-
-Primum spoliata satis. His unaque tutaeque et quaeris pater quo currere teneri.
-Emicuit retinacula opta. Sex heros mira rursus fui viscere deieci, pecudis
-venis.
-
-1. Mentitis viribus crescendo torquet
-2. Cum iurasse paludes iuvenalibus opemque
-3. Reliquit hostem
-4. Neci genu sinunt unus
-
-Vere at fulmen verba quoque lustra duarum perdideris, comitant et colus: quam
-iam inpia sociasque? Turbata cetera, fui petens, orbem animoque et trahit et
-currere, virgineis. Furta celeremque relevare et formam loca dextra dis,
-[luce](http://sola.org/comminus-retexi) mos procul iam nefas? Ignotissima amnem
-vis diligit Venus, luctante, non aquas, qua hunc est membra, confusaque colorem
-posses. Vocabitur resumptis orbem!
-
-- Erat te laboris facta adiere sedes
-- Sede novas bracchia memoratis alios
-- Bella versis
-- Me membra Romam contigit tacitaque huius
-
-In pulvere saepe exanimesque fortisque bibulis vos obsedit, elementa aeterna
-caerulus. Lumina Circaea illas, cum sub motus: excepit quam mandasset peremptam
-pedum, non latet rursusque adspexit pars Salamis. Aduncos inmisit involvitur
-plangore regna cupressus et thalami neque inculpata.
-
-- [ ] test
-- [x] test
-''');
-
         return Padding(
           padding: EdgeInsets.symmetric(
             horizontal: MediaQuery.of(context).size.width * UpdateRoute.paddingFactor,
