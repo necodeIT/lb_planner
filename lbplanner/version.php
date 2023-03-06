03074a8bc15f88e17e657f9d1954b58c50072b18
mkocagoel <muhiko68@gmail.com>
2023-03-06 10:16:52 +0100

    adding the right version

--- lbplanner/version.php
+++ lbplanner/version.php
@@ -20,6 +20,6 @@ $release = '0.0.0';
 
 $plugin->component = 'local_lbplanner';
 $plugin->release = 'Alpha v.'.$release;
-$plugin->version = 20230119011;
+$plugin->version = 2023030601;
 
 set_config('release', $release, 'local_lbplanner');
