517c44ee55064fb0c5fa2a6976c302d81d10c7ec
RiedleroD <github@riedler.wien>
2023-03-09 16:05:13 +0100

    formatted docs index

--- docs/index.php
+++ docs/index.php
@@ -1,62 +1,63 @@
 <?php
 	const root = '..';
 	const cursite = 2;
-	
+
 	require_once('../snippets/snips.php');
 	require_once('../snippets/db.php');
 	require_once('../snippets/docs.php');
-	
+
 	setContext();
-	
-	$stylesheets = array("main","docs");
-	$scripts = array("docs");
+
+	$stylesheets = ['main', 'docs'];
+	$scripts = ['docs'];
 	include('../snippets/head.php');
-	
-	if(key_exists('section',$_GET)){
+
+	if (key_exists('section', $_GET)) {
 		$wanted_section = $_GET['section'];
-	}else{
+	} else {
 		$wanted_section = 0;
 	}
-	if(key_exists('heading',$_GET)){
+	if (key_exists('heading', $_GET)) {
 		$wanted_heading = $_GET['heading'];
-	}else{
+	} else {
 		$wanted_heading = -1;
 	}
-	
+
 	$stuff = db_get_docs_stuff();
 ?>
-	<body class="theme-<?php echo $context_theme; ?>">
+
+	<body class="theme-<?= $context_theme; ?>">
 		<?php
 			include('../snippets/navbar.php');
 		?>
 		<div id="sidebar">
 			<?php
-				foreach($stuff as $title){
+				foreach ($stuff as $title) {
 					echo "<label for=\"radio_title{$title->id}\">{$title->text}</label>";
 				}
 			?>
 		</div>
 		<?php
-			foreach($stuff as $title){
-				echo '<input type="radio" id="radio_title'.($title->id);
-				if($wanted_section==$title->id){
-					echo '" checked name="pageradios"/>';//page radios not pager adios
-				}else{
+			foreach ($stuff as $title) {
+				echo '<input type="radio" id="radio_title'.$title->id;
+				if ($wanted_section == $title->id) {
+					echo '" checked name="pageradios"/>'; // page radios not pager adios
+				} else {
 					echo '" name="pageradios"/>';
 				}
 				echo '<div class="scrollwrap"><div class="allpage">';
-				foreach($title->headings as $heading){
-					?>
-					<details<?=$wanted_heading==$heading->id?' open':''?>>
+				foreach ($title->headings as $heading) {
+		?>
+					<details<?= $wanted_heading == $heading->id ? ' open' : ''; ?>>
 						<summary><?php
 							echo $heading->text;
-							include("../resources/dropdown_arrow.svg");
+							include('../resources/dropdown_arrow.svg');
 						?></summary>
 						<div class="p">
-							<?php docs_content_pp($heading->body);?>
+							<?php docs_content_pp($heading->body); ?>
 						</div>
 					</details>
-					<?php
+			<?php
 				}
 				echo '</div></div>';
 			}
