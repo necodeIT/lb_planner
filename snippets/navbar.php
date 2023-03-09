842b4cd8d29f956deba944148052e606768e437e
RiedleroD <github@riedler.wien>
2023-03-09 17:25:13 +0100

    formatted navbar.php

--- snippets/navbar.php
+++ snippets/navbar.php
@@ -1,12 +1,12 @@
 <?php
-	require_once(root.'/snippets/db.php');
+
+require_once(root.'/snippets/db.php');
 	
-	$c=array('','','');
-	$c[cursite-1]=' class="current"';
-	echo '<nav id="mainnav">';
-		include(root."/resources/wordmark.svg");
-		echo '<a href="'.urlroot.'/"'.$c[0].'>'.GDS('nav_dl').'</a>';
-		echo '<a href="'.urlroot.'/docs/"'.$c[1].'>'.GDS('nav_doc').'</a>';
-		#echo '<a href="'.urlroot.'/roadmap/"'.$c[2].'>'.GDS('nav_road').'</a>';
-	echo '</nav>';
-?>
+$c = ['', '', ''];
+$c[cursite - 1] = ' class="current"';
+echo '<nav id="mainnav">';
+include(root.'/resources/wordmark.svg');
+echo '<a href="'.urlroot.'/"'.$c[0].'>'.GDS('nav_dl').'</a>';
+echo '<a href="'.urlroot.'/docs/"'.$c[1].'>'.GDS('nav_doc').'</a>';
+// echo '<a href="'.urlroot.'/roadmap/"'.$c[2].'>'.GDS('nav_road').'</a>';
+echo '</nav>';
