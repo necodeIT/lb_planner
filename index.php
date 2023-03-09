7906ab0565c150289aa5701fe3858dc49df5619d
RiedleroD <github@riedler.wien>
2023-03-09 17:27:53 +0100

    formatted index.php

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
 <?php
-	const root = '.';
-	const cursite = 1;
+const root = '.';
+const cursite = 1;
 	
-	require_once('./snippets/snips.php');
-	require_once('./snippets/db.php');
-	require_once('./snippets/download.php');
+require_once('./snippets/snips.php');
+require_once('./snippets/db.php');
+require_once('./snippets/download.php');
 	
-	setContext();
+setContext();
 	
-	$stylesheets = array("main","download");
-	include('./snippets/head.php');
+$stylesheets = ['main', 'download'];
+include('./snippets/head.php');
 ?>
-	<body class="theme-<?php echo $context_theme; ?>">
-		<?php 
+	<body class="theme-<?= $context_theme; ?>">
+		<?php
 			include('./snippets/navbar.php');
 		?>
 		<section class="screenpage" id="og1">
-			<h1><?php EDS('title_main') ?></h2>
-			<h3><?php EDS('span_main') ?></h3>
+			<h1><?php EDS('title_main'); ?></h2>
+			<h3><?php EDS('span_main'); ?></h3>
 			<span>
 				<?php
-					genDownloadButton($context_os);
-					genExtRef(GDS('lnk_docs'),urlroot.'/docs/');
-					#genExtRef(GDS('lnk_admin'),urlroot.'/docs/#admin');
+				genDownloadButton($context_os);
+				genExtRef(GDS('lnk_docs'), urlroot.'/docs/');
+				// genExtRef(GDS('lnk_admin'),urlroot.'/docs/#admin');
 				?>
 			</span>
-			<img class="card" src="<?=urlroot?>/resources/example_cal.png" alt="example screenshot"/>
-			<h1 class="sectionheader"><?php EDS('title_theme') ?></h1>
+			<img class="card" src="<?= urlroot; ?>/resources/example_cal.png" alt="example screenshot"/>
+			<h1 class="sectionheader"><?php EDS('title_theme'); ?></h1>
 			<div id="themebox">
 				<div>
 					<?php
@@ -41,35 +41,35 @@
 					?>
 				</div>
 			</div>
-			<span><?php EDS('p_themes1');EDS('p_themes2') ?></span>
+			<span><?php EDS('p_themes1'); EDS('p_themes2'); ?></span>
 		</section>
 		<?php
-			$images = array('dashboard','calendar','calendar_modules_overview');
-			foreach(array(1,2,3) as $i){
-				genFeatureSection(
-					GDS('title_demo'.$i),
-					MGDS("p_demo${i}",[1,2]),
-					$images[$i-1]
-				);
-			}
+		$images = ['dashboard', 'calendar', 'calendar_modules_overview'];
+		foreach ([1, 2, 3] as $i) {
+			genFeatureSection(
+				GDS('title_demo'.$i),
+				MGDS("p_demo{$i}", [1, 2]),
+				$images[$i - 1]
+			);
+		}
 		?>
 		<section class="screenpage" id="ofeat">
-			<h1 class="sectionheader"><?php EDS('title_features') ?></h1>
+			<h1 class="sectionheader"><?php EDS('title_features'); ?></h1>
 			<?php
-				genLearnCard('plan',1,5,40);
-				genLearnCard('overview',2,0,2);
-				genLearnCard('sync',3,0,5);
+			genLearnCard('plan', 1, 5, 40);
+			genLearnCard('overview', 2, 0, 2);
+			genLearnCard('sync', 3, 0, 5);
 			?>
 			<hr/>
 			<?php
-				genDownloadCard('linux');
-				genDownloadCard('windows');
-				genDownloadCard('mac');
+			genDownloadCard('linux');
+			genDownloadCard('windows');
+			genDownloadCard('mac');
 			?>
-			<p><?php MEDS('p_download',[1,2]) ?></p>
+			<p><?php MEDS('p_download', [1, 2]); ?></p>
 		</section>
 		<?php
-			include('./snippets/foot.php');
+		include('./snippets/foot.php');
 		?>
 	</body>
 </html>
