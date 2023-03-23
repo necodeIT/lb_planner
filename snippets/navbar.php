14292824407a0e8fb001210f22ea3207c918435a
RiedleroD <github@riedler.wien>
2023-03-23 16:41:07 +0100

    made logo in header clickable fixes LB-68

--- snippets/navbar.php
+++ snippets/navbar.php
@@ -4,9 +4,10 @@ require_once(root.'/snippets/db.php');
 	
 $c = ['', '', ''];
 $c[cursite - 1] = ' class="current"';
-echo '<nav id="mainnav">';
+echo '<nav id="mainnav"><a href="/" class="logo">';
 include(root.'/resources/wordmark.svg');
-echo '<a href="'.urlroot.'/"'.$c[0].'>'.GDS('nav_dl').'</a>';
-echo '<a href="'.urlroot.'/docs/"'.$c[1].'>'.GDS('nav_doc').'</a>';
+echo '</a>';
+echo '<a class="navlink" href="'.urlroot.'/"'.$c[0].'>'.GDS('nav_dl').'</a>';
+echo '<a class="navlink" href="'.urlroot.'/docs/"'.$c[1].'>'.GDS('nav_doc').'</a>';
 // echo '<a href="'.urlroot.'/roadmap/"'.$c[2].'>'.GDS('nav_road').'</a>';
 echo '</nav>';
