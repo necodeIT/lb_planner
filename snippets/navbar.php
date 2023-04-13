bc53ac10a9e4cd126caa652d4199c1b4cbbacdb7
RiedleroD <github@riedler.wien>
2023-04-13 15:53:34 +0200

    navigate to the download section when clicked on the "Download" navbar item closes LP-82

--- snippets/navbar.php
+++ snippets/navbar.php
@@ -7,7 +7,7 @@ $c[cursite - 1] = ' current';
 echo '<nav id="mainnav"><a href="'.urlroot.'" class="logo">';
 include(root.'/resources/wordmark.svg');
 echo '</a>';
-echo "<a class=\"navlink {$c[0]}\" href=\"".urlroot."/\">".GDS('nav_dl').'</a>';
+echo "<a class=\"navlink {$c[0]}\" href=\"".urlroot."/#{$context_os}dlcard\">".GDS('nav_dl').'</a>';
 echo "<a class=\"navlink {$c[1]}\" href=\"".urlroot."/docs/\">".GDS('nav_doc').'</a>';
 // echo '<a href="'.urlroot.'/roadmap/"'.$c[2].'>'.GDS('nav_road').'</a>';
 echo '</nav>';
