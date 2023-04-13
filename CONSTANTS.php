5e513a0df92d7f457ab46febcfbefebc8d0586f4
RiedleroD <github@riedler.wien>
2023-04-13 16:08:04 +0200

    fixed navbar logo not redirecting to main page when urlroot is empty

--- CONSTANTS.php
+++ CONSTANTS.php
@@ -3,9 +3,10 @@
 const supported_languages = array('en','de');
 const supported_systems = array('linux','mac','windows');
 
-// enter your url root here
+// enter your url root here, with preceding, but no trailing slash
 // if your base url is e.g. "https://projekte.tgm.ac.at/lb-planner/", then
 // this should be "/lb-planner"
+// if your base url is "https://lbplanner.local/", then this should be "/"
 const urlroot = "";
 
 // database details
--- snippets/navbar.php
+++ snippets/navbar.php
@@ -4,7 +4,7 @@ require_once(root.'/snippets/db.php');
 	
 $c = ['', ''];
 $c[cursite - 1] = ' current';
-echo '<nav id="mainnav"><a href="'.urlroot.'" class="logo">';
+echo '<nav id="mainnav"><a href="'.urlroot.'/" class="logo">';
 include(root.'/resources/wordmark.svg');
 echo '</a>';
 echo "<a class=\"navlink {$c[0]}\" href=\"".urlroot."/#{$context_os}dlcard\">".GDS('nav_dl').'</a>';
