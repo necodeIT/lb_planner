7a6cdbc1e130f5a7199d593b8cb7dc115afe620c
mkocagoel <muhammed@kocagoel.at>
2023-07-22 19:14:37 +0200

    initializing a few  files

---
+++ lbplanner/classes/helpers/slotbooking_helper.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of the local_lbplanner.
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace local_lbplanner\helpers;
+
+enum weekdays {
+    case monday = 1;
+    case tuesday = 2;
+    case wednesday = 3;
+    case thursday = 4;
+    case friday = 5;
+    case saturday = 6;
+    case sunday = 7;
+}
+
+class slotbooking_helper{
+
+}
