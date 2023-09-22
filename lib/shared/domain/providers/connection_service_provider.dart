50a041b1d6133c2669db40651e52785002754f0f
bmceachnie <60017181+McQuenji@users.noreply.github.com>
2023-09-22 13:48:28 +0200

    renamed AutoRefreshStateNotifier's file & fixed some comments+

--- lib/shared/domain/providers/connection_service_provider.dart
+++ lib/shared/domain/providers/connection_service_provider.dart
@@ -1,14 +1,12 @@
 import 'dart:async';
 
 import 'package:lb_planner/shared/domain/services/internet_address_lookup_connection_service.dart';
-import 'package:lb_planner/shared/utils/auto_refresh_provider.dart';
+import 'package:lb_planner/shared/utils/auto_refresh_state_notifier.dart';
 import 'package:riverpod/riverpod.dart';
 import 'package:lb_planner/shared/domain/services/services.dart';
 
-/// Provides the current [NetworkService] instance.
-///
-/// See [NetworkService] for more information.
-final netwokServiceProvider = Provider<_ConnectionProvider>((ref) {
+/// Provides the current connection status of the device.
+final connectionServiceProvider = Provider<_ConnectionProvider>((ref) {
   final connectionService = InternetAdressLookupConnectionService();
 
   return _ConnectionProvider(connectionService);
--- lib/shared/domain/providers/providers.dart
+++ lib/shared/domain/providers/providers.dart
@@ -1,1 +1,2 @@
 export 'network_service_provider.dart';
+export 'connection_service_provider.dart';
