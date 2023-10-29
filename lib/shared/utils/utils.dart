cf8a4f426d8221f1f42c95c8e669c07c93e9311b
bmceachnie <60017181+McQuenji@users.noreply.github.com>
2023-10-30 00:13:07 +0100

    cleaned up docs

--- lib/shared/utils/utils.dart
+++ lib/shared/utils/utils.dart
@@ -1,3 +1,3 @@
 export 'auto_refresh_state_notifier.dart';
 export 'string_utils.dart';
-export 'package:collection/collection.dart';
+export 'package:collection/collection.dart' show IterableExtension;
