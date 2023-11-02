ae2c2387929bdfb863728c94cea01ff79c285ce4
bmceachnie <60017181+McQuenji@users.noreply.github.com>
2023-11-02 22:52:58 +0100

    cleanup + minor bug fix

--- lib/shared/domain/models/models.dart
+++ lib/shared/domain/models/models.dart
@@ -1,7 +1,6 @@
 export 'http_response.dart';
 export 'version.dart';
 export 'install_medium.dart';
-export 'default_route_data.dart';
 
 /// A type alias for JSON objects.
 typedef JSON = Map<String, dynamic>;
