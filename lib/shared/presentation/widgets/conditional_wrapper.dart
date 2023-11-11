b741bd33b08387f15fbc6cfea76f075b505f3883
Can42 <canmp42@hotmail.com>
2023-11-11 19:18:08 +0100

    Update conditional_wrapper.dart

--- lib/shared/presentation/widgets/conditional_wrapper.dart
+++ lib/shared/presentation/widgets/conditional_wrapper.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 /// Allows you to conditionally wrap a [child] widget with one of two different wrappers based on a specified [condition].
 ///
-/// You can provide a[wrapper] to use when the [condition] is `true` and an optional [falseWrapper] to use when the [condition] is `false`.
+/// You can provide a [wrapper] to use when the [condition] is `true` and an optional [falseWrapper] to use when the [condition] is `false`.
 /// This is useful for applying conditional styling or functionality to other widgets.
 class ConditionalWrapper extends StatelessWidget {
   /// Allows you to conditionally wrap a [child] widget with one of two different wrappers based on a specified [condition].
   ///
-  /// You can provide a[wrapper] to use when the [condition] is `true` and an optional [falseWrapper] to use whenthe [condition] is `false`.
+  /// You can provide a [wrapper] to use when the [condition] is `true` and an optional [falseWrapper] to use whenthe [condition] is `false`.
   /// This is useful for applying conditional styling or functionality to other widgets.
   const ConditionalWrapper(
       {Key? key,
