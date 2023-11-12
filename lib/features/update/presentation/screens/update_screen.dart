5282f5d1a90111946e844d7f2ba2e512717da13d
Can42 <canmp42@hotmail.com>
2023-11-12 03:20:53 +0100

    cproject structure created

---
+++ lib/features/update/presentation/screens/update_screen.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lb_planner/shared/shared.dart';
+
+/// Update route.
+
+@RoutePage()
+class UpdateScreen extends StatefulWidget {
+  /// Update route.
+  const UpdateScreen({Key? key}) : super(key: key);
+
+  /// Factor multiplied by the screen width to get the padding.
+  static const paddingFactor = 0.1;
+
+  /// Size of the app icon.
+  static const double iconSize = 60;
+
+  @override
+  State<UpdateScreen> createState() => _UpdateScreenState();
+}
+
+class _UpdateScreenState extends State<UpdateScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (context, ref, _) {
+        var updater = ref.watch(updateController);
+        var update = ref.watch(updateProvider);
+
+        void showCommand() {
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            lpShowAlertDialog(
+              context,
+              title: t.update_dialog_title,
+              body: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  NcBodyText(
+                    kSetupType.canAutoUpdate
+                        ? t.update_dialog_helpNeeded
+                        : t.update_dialog_noAutoUpdate,
+                    overflow: TextOverflow.visible,
+                  ),
+                  Spacing.large(),
+                  LpSnippet(code: update.command),
+                ],
+              ),
+            );
+          });
+        }
+
+        if (update.command.isNotEmpty) {
+          showCommand();
+        }
+
+        if (update.downloadStatus == DownloadStatus.error) {
+          Catcher.reportCheckedError(
+              kUpdateErrorKeyword + update.error, StackTrace.current);
+        }
+
+        return Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal:
+                MediaQuery.of(context).size.width * UpdateScreen.paddingFactor,
+            vertical:
+                MediaQuery.of(context).size.height * UpdateScreen.paddingFactor,
+          ),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      VectorImage('assets/svg/app_icon.svg',
+                          height: UpdateScreen.iconSize),
+                      Spacing.small(),
+                      NcTitleText(
+                        t.update_patchNotes(update.latestVersionName),
+                        fontSize: RouteTitle.titleSize,
+                      ),
+                    ],
+                  ),
+                  if (update.downloadStatus == DownloadStatus.idle)
+                    Button(
+                      onPressed: updater.upgrade,
+                      text: t.update_btn,
+                    ),
+                  if (update.downloadStatus == DownloadStatus.error)
+                    Button(
+                      onPressed: updater.upgrade,
+                      text: t.update_btnErr,
+                    ),
+                  if (update.downloadStatus == DownloadStatus.downlaoding)
+                    Row(
+                      children: [
+                        NcBodyText(
+                          t.update_downloading(
+                              update.fileName, update.progress),
+                        ),
+                        Spacing.small(),
+                        CircularProgressIndicator(),
+                      ],
+                    ),
+                  if (update.downloadStatus == DownloadStatus.installing &&
+                      update.command.isEmpty)
+                    NcBodyText(t.update_installing),
+                  if (update.downloadStatus == DownloadStatus.installing &&
+                      update.command.isNotEmpty)
+                    LpButton(onPressed: showCommand, text: t.update_btnInstall),
+                ],
+              ),
+              Spacing.large(),
+              Expanded(
+                child: Markdown(update.patchNotes),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/shared/presentation/widgets/localized_widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+/// Provides localized strings for the app.
+abstract class LocalizedWidget extends StatefulWidget {
+  /// Provides localized strings for the app.
+  const LocalizedWidget({Key? key}) : super(key: key);
+
+  /// Creates the widget.
+  Widget build(BuildContext context, AppLocalizations t);
+
+  @override
+  State<LocalizedWidget> createState() => _LocalizedWidgetState();
+}
+
+class _LocalizedWidgetState extends State<LocalizedWidget> {
+  @override
+  Widget build(BuildContext context) => widget.build(context, t);
+}
+
+/// Provides localized strings for the app.
+extension ContextLocalization on BuildContext {
+  /// Provides localized strings for the app.
+  AppLocalizations get t => AppLocalizations.of(this)!;
+}
+
+/// Provides localized strings for the app.
+extension StateLocalization<T extends StatefulWidget> on State<T> {
+  /// Provides localized strings for the app.
+  AppLocalizations get t => context.t;
+}
