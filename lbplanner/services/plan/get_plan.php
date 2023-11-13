6374c2d795c08bd7c41a117c1036fef5799f5374
RiedleroD <github@riedler.wien>
2023-11-13 13:07:41 +0100

    refactored get_plan

--- lbplanner/services/plan/get_plan.php
+++ lbplanner/services/plan/get_plan.php
@@ -18,36 +18,20 @@ namespace local_lbplanner_services;
 
 use external_api;
 use external_function_parameters;
-use external_value;
 use local_lbplanner\helpers\plan_helper;
-use local_lbplanner\helpers\user_helper;
 
 /**
  * Get the plan of the given user.
  */
 class plan_get_plan extends external_api {
     public static function get_plan_parameters() {
-        return new external_function_parameters(array(
-            'userid' => new external_value(
-                PARAM_INT,
-                'The id of the user',
-                VALUE_REQUIRED,
-                null,
-                NULL_NOT_ALLOWED
-            ),
-        ));
+        return new external_function_parameters(array());
     }
 
-    public static function get_plan($userid) {
-        global $DB;
+    public static function get_plan() {
+        global $DB, $USER;
 
-        self::validate_parameters(self::get_plan_parameters(), array('userid' => $userid));
-
-        user_helper::assert_access($userid);
-
-        $planid = plan_helper::get_plan_id($userid);
-
-        $plan = $DB->get_record(plan_helper::TABLE, array('id' => $planid));
+        $planid = plan_helper::get_plan_id($USER->id);
 
         return plan_helper::get_plan($planid);
     }
