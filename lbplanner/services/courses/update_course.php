6ddb764dfd7f51c7edcf27bb669a594710c71026
Muhi <muhiko68@gmail.com>
2023-11-15 18:11:29 +0100

    Reworked update_course.php

--- lbplanner/services/courses/update_course.php
+++ lbplanner/services/courses/update_course.php
@@ -16,12 +16,13 @@
 
 namespace local_lbplanner_services;
 
+use dml_exception;
 use external_api;
 use external_function_parameters;
-use external_single_structure;
 use external_value;
-use local_lbplanner\helpers\user_helper;
+use invalid_parameter_exception;
 use local_lbplanner\helpers\course_helper;
+use moodle_exception;
 
 /**
  * Update the data for a course.
@@ -31,7 +32,7 @@ class courses_update_course extends external_api {
      * Parameters for update_course.
      * @return external_function_parameters
      */
-    public static function update_course_parameters() {
+    public static function update_course_parameters(): external_function_parameters {
         return new external_function_parameters([
             'courseid' => new external_value(PARAM_INT, 'The id of the course', VALUE_REQUIRED, null, NULL_NOT_ALLOWED),
             'color' => new external_value(PARAM_TEXT, 'The color of the course', VALUE_DEFAULT, null),
@@ -52,11 +53,11 @@ class courses_update_course extends external_api {
      * @param string $shortname The shortname of the course
      * @param bool $enabled Whether the course is enabled or not
      * @return void
-     * @throws \dml_exception
-     * @throws \invalid_parameter_exception
-     * @throws \moodle_exception
+     * @throws dml_exception
+     * @throws invalid_parameter_exception
+     * @throws moodle_exception
      */
-    public static function update_course($courseid, $color, $shortname, $enabled) {
+    public static function update_course( $courseid, $color, $shortname, $enabled): void {
         global $DB , $USER;
 
         self::validate_parameters(
@@ -70,7 +71,7 @@ class courses_update_course extends external_api {
         );
 
         if (strlen($shortname) > 5) {
-            throw new \moodle_exception('Shortname is too long');
+            throw new moodle_exception('Shortname is too long');
         }
 
         $course = course_helper::get_lbplanner_course($courseid, $USER->id);
