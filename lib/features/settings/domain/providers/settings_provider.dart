7b1f514dc3eeaae6b91b5bd529d787a6189649aa
bmceachnie <60017181+McQuenji@users.noreply.github.com>
2023-11-20 14:01:37 +0100

    fixed some typos

--- lib/features/settings/domain/providers/settings_provider.dart
+++ lib/features/settings/domain/providers/settings_provider.dart
@@ -5,7 +5,7 @@ import 'package:lb_planner/shared/shared.dart';
 
 /// Provides methods to change the [User]'s settings.
 ///
-/// **NOTE:** This provider only provides methods to change the settings, as the current settings can be infered from the [User] object.
+/// **NOTE:** This provider only provides methods to change the settings, as the current settings can be inferred from the [User] object.
 ///
 /// If you want to get the current settings, see [userProvider].
 final settingsProvider = Provider<SettingsProviderState>((ref) {
--- lib/features/settings/domain/providers/settings_provider_state.dart
+++ lib/features/settings/domain/providers/settings_provider_state.dart
@@ -4,7 +4,7 @@ import 'package:lb_planner/shared/shared.dart';
 
 /// Provides methods to change the [User]'s settings.
 ///
-/// **NOTE:** This provider only provides methods to change the settings, as the current settings can be infered from the [User] object.
+/// **NOTE:** This provider only provides methods to change the settings, as the current settings can be inferred from the [User] object.
 ///
 /// If you want to get the current settings, see [userProvider].
 class SettingsProviderState {
@@ -21,7 +21,7 @@ class SettingsProviderState {
 
   /// Provides methods to change the [User]'s settings.
   ///
-  /// **NOTE:** This provider only provides methods to change the settings, as the current settings can be infered from the [User] object.
+  /// **NOTE:** This provider only provides methods to change the settings, as the current settings can be inferred from the [User] object.
   ///
   /// If you want to get the current settings, see [userProvider].
   SettingsProviderState(this.user, this.controller, this.appDirService);
