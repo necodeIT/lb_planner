d46715baee2844bc6572a4e57e5dee9604b98125
Can Polat <74594491+cpolat-tgm@users.noreply.github.com>
2023-11-21 15:18:29 +0100

    Update user_notifications_item.dart

--- lib/features/notifications/presentation/widgets/user_notifications_item.dart
+++ lib/features/notifications/presentation/widgets/user_notifications_item.dart
@@ -43,219 +43,214 @@ class _UserNotificationsItemState extends ConsumerState<UserNotificationsItem> {
 
   @override
   Widget build(context) {
-    return Consumer(
-      builder: (context, ref, _) {
-        var notification =
-            ref.watch(notificationsProvider)[widget.notificationId];
+    var notification = ref.watch(notificationsProvider)[widget.notificationId];
 
-        String text = notification.toString();
-        List<_Action> actions = [];
-        bool loading = false;
+    String text = notification.toString();
+    List<_Action> actions = [];
+    bool loading = false;
 
-        switch (notification.type) {
-          case NotificationTypes.invite:
-            int id = notification.payload!;
+    switch (notification.type) {
+      case NotificationType.invite:
+        int id = notification.payload!;
 
-            var invite = ref.watch(invitesProvider)[id];
+        var invite = ref.watch(invitesProvider)[id];
 
-            if (invite == null) {
-              loading = true;
-              break;
-            }
-
-            var controller = ref.read(invitesController);
-
-            var inviter = ref.watch(usersProvider)[invite.inviter];
-
-            var plan = ref.watch(planProvider);
-
-            if (inviter == null) {
-              loading = true;
-              break;
-            }
-
-            text = t.user_notifications_invite_text(inviter.fullname);
-
-            actions = [
-              if (invite.status.isPending && _future == null)
-                _Action(
-                  text: t.user_notifications_invite_accept,
-                  onPressed: () {
-                    void action() {
-                      load(
-                        [
-                          controller.acceptInvite(invite.id),
-                          ref.read(planController).fetchPlan(),
-                        ],
-                      );
-                    }
-
-                    if (plan.deadlines.isEmpty) return action();
-
-                    lpShowConfirmDialog(
-                      context,
-                      title: t.user_notifications_invite_accept_confirm_title,
-                      message: t.user_notifications_invite_accept_confirm_body,
-                      confirmIsBad: true,
-                      onConfirm: action,
-                    );
-                  },
-                ),
-              if (invite.status.isPending && _future == null)
-                _Action(
-                  text: t.user_notifications_invite_decline,
-                  onPressed: () => load([controller.declineInvite(invite.id)]),
-                ),
-              if (!invite.status.isPending)
-                _Action(
-                  text: invite.status.isExpired
-                      ? t.user_notifications_invite_expired
-                      : invite.status.isAccepted
-                          ? t.user_notifications_invite_accepted
-                          : t.user_notifications_invite_declined,
-                  loading: _future != null,
-                ),
-            ];
-
-            break;
-          case NotificationTypes.inviteAccepted:
-            var id = notification.payload;
-
-            var invite = ref.watch(invitesProvider)[id];
-
-            if (invite == null) {
-              loading = true;
-              break;
-            }
-
-            var user = ref.watch(usersProvider)[invite.invitee];
-
-            if (user == null) {
-              loading = true;
-              break;
-            }
-
-            text = t.user_notifications_inviteAccepted_text(user.fullname);
-            break;
-          case NotificationTypes.inviteDeclined:
-            var id = notification
-                .payload; // dunno why but "inviteid" is appearently a string
-
-            var invite = ref.watch(invitesProvider)[id];
-
-            if (invite == null) {
-              loading = true;
-              break;
-            }
-
-            var user = ref.watch(usersProvider)[invite.invitee];
-
-            if (user == null) {
-              loading = true;
-              break;
-            }
-
-            text = t.user_notifications_inviteDeclined_text(user.fullname);
-
-            break;
-          case NotificationTypes.planLeft:
-            var userid = notification.payload;
-            var users = ref.watch(usersProvider);
-
-            var user = users[userid];
-
-            if (user == null) {
-              loading = true;
-              break;
-            }
-
-            text = t.user_notifications_planLeft_text(user.firstname);
-            break;
-          case NotificationTypes.planRemoved:
-            var userId = notification.payload;
-
-            var user = ref.watch(usersProvider)[userId];
-
-            if (user == null) {
-              loading = true;
-              break;
-            }
-
-            text = t.user_notifications_planRemoved_text(user.fullname);
-            break;
-          case NotificationTypes.userRegistered:
-            var user = ref.read(userProvider);
-
-            text = t.user_notifications_userRegistered_text(user.firstname);
-
-            var userTheme = NcThemes.current.name == sakuraTheme.name
-                ? "sakura"
-                : NcThemes.current.name.toLowerCase();
-
-            var userLang = user.language.isEn ? 'en' : 'de';
-            var urlToLaunch =
-                'https://projekte.tgm.ac.at/lb-planner/docs/?theme=$userTheme&lang=$userLang&section=0&heading=2';
-
-            actions = [
-              _Action(
-                  text: t.user_notifications_userRegistered_docs,
-                  // When pressed, open the docs in a browser
-                  onPressed: () => launchUrl(Uri.parse(urlToLaunch))),
-            ];
-
-            break;
+        if (invite == null) {
+          loading = true;
+          break;
         }
 
-        return ConditionalWidget(
-          condition: loading,
-          trueWidget: (context) => LpShimmer(),
-          falseWidget: (context) => Container(
-            padding: EdgeInsets.all(NcSpacing.smallSpacing),
-            decoration: BoxDecoration(
-              color: secondaryColor,
-              borderRadius: BorderRadius.circular(kRadius),
+        var controller = ref.read(invitesController);
+
+        var inviter = ref.watch(usersProvider)[invite.inviter];
+
+        var plan = ref.watch(planProvider);
+
+        if (inviter == null) {
+          loading = true;
+          break;
+        }
+
+        text = t.user_notifications_invite_text(inviter.fullname);
+
+        actions = [
+          if (invite.status.isPending && _future == null)
+            _Action(
+              text: t.user_notifications_invite_accept,
+              onPressed: () {
+                void action() {
+                  load(
+                    [
+                      controller.acceptInvite(invite.id),
+                      ref.read(planController).fetchPlan(),
+                    ],
+                  );
+                }
+
+                if (plan.deadlines.isEmpty) return action();
+
+                lpShowConfirmDialog(
+                  context,
+                  title: t.user_notifications_invite_accept_confirm_title,
+                  message: t.user_notifications_invite_accept_confirm_body,
+                  confirmIsBad: true,
+                  onConfirm: action,
+                );
+              },
             ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
+          if (invite.status.isPending && _future == null)
+            _Action(
+              text: t.user_notifications_invite_decline,
+              onPressed: () => load([controller.declineInvite(invite.id)]),
+            ),
+          if (!invite.status.isPending)
+            _Action(
+              text: invite.status.isExpired
+                  ? t.user_notifications_invite_expired
+                  : invite.status.isAccepted
+                      ? t.user_notifications_invite_accepted
+                      : t.user_notifications_invite_declined,
+              loading: _future != null,
+            ),
+        ];
+
+        break;
+      case NotificationType.inviteAccepted:
+        var id = notification.payload;
+
+        var invite = ref.watch(invitesProvider)[id];
+
+        if (invite == null) {
+          loading = true;
+          break;
+        }
+
+        var user = ref.watch(usersProvider)[invite.invitee];
+
+        if (user == null) {
+          loading = true;
+          break;
+        }
+
+        text = t.user_notifications_inviteAccepted_text(user.fullname);
+        break;
+      case NotificationType.inviteDeclined:
+        var id = notification
+            .payload; // dunno why but "inviteid" is appearently a string
+
+        var invite = ref.watch(invitesProvider)[id];
+
+        if (invite == null) {
+          loading = true;
+          break;
+        }
+
+        var user = ref.watch(usersProvider)[invite.invitee];
+
+        if (user == null) {
+          loading = true;
+          break;
+        }
+
+        text = t.user_notifications_inviteDeclined_text(user.fullname);
+
+        break;
+      case NotificationType.planLeft:
+        var userid = notification.payload;
+        var users = ref.watch(usersProvider);
+
+        var user = users[userid];
+
+        if (user == null) {
+          loading = true;
+          break;
+        }
+
+        text = t.user_notifications_planLeft_text(user.firstname);
+        break;
+      case NotificationType.planRemoved:
+        var userId = notification.payload;
+
+        var user = ref.watch(usersProvider)[userId];
+
+        if (user == null) {
+          loading = true;
+          break;
+        }
+
+        text = t.user_notifications_planRemoved_text(user.fullname);
+        break;
+      case NotificationType.userRegistered:
+        var user = ref.read(userProvider);
+
+        text = t.user_notifications_userRegistered_text(user.firstname);
+
+        var userTheme = NcThemes.current.name == sakuraTheme.name
+            ? "sakura"
+            : NcThemes.current.name.toLowerCase();
+
+        var userLang = user.language.isEn ? 'en' : 'de';
+        var urlToLaunch =
+            'https://projekte.tgm.ac.at/lb-planner/docs/?theme=$userTheme&lang=$userLang&section=0&heading=2';
+
+        actions = [
+          _Action(
+              text: t.user_notifications_userRegistered_docs,
+              // When pressed, open the docs in a browser
+              onPressed: () => launchUrl(Uri.parse(urlToLaunch))),
+        ];
+
+        break;
+    }
+
+    return ConditionalWidget(
+      condition: loading,
+      trueWidget: (context) => LpShimmer(),
+      falseWidget: (context) => Container(
+        padding: EdgeInsets.all(NcSpacing.smallSpacing),
+        decoration: BoxDecoration(
+          color: secondaryColor,
+          borderRadius: BorderRadius.circular(kRadius),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            NcCaptionText(
+              text,
+              overflow: TextOverflow.visible,
+            ),
+            NcSpacing.small(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                NcCaptionText(
-                  text,
-                  overflow: TextOverflow.visible,
-                ),
-                NcSpacing.small(),
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Row(
-                      children: [
-                        LpIcon(
-                          Icons.access_time,
-                          size: UserNotificationsItem.actionsFontSize,
-                          // ignore: no-magic-number
-                          color: textColor.withOpacity(.7),
-                        ),
-                        NcSpacing.xs(),
-                        NcCaptionText(
-                          timeago.format(notification.timestamp),
-                          // ignore: no-magic-number
-                          color: textColor.withOpacity(.7),
-                          fontSize: UserNotificationsItem.actionsFontSize,
-                        ),
-                      ],
+                    LpIcon(
+                      Icons.access_time,
+                      size: UserNotificationsItem.actionsFontSize,
+                      // ignore: no-magic-number
+                      color: textColor.withOpacity(.7),
                     ),
-                    Row(
-                      children: [
-                        for (var action in actions) action.build(context),
-                      ],
+                    NcSpacing.xs(),
+                    NcCaptionText(
+                      timeago.format(notification.timestamp),
+                      // ignore: no-magic-number
+                      color: textColor.withOpacity(.7),
+                      fontSize: UserNotificationsItem.actionsFontSize,
                     ),
                   ],
-                )
+                ),
+                Row(
+                  children: [
+                    for (var action in actions) action.build(context),
+                  ],
+                ),
               ],
-            ),
-          ),
-        );
-      },
+            )
+          ],
+        ),
+      ),
     );
   }
 }
