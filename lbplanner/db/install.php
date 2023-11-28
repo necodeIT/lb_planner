21b8d1b0b8dd39d76066403cc19b89f2e573ef1a
Muhi <muhiko68@gmail.com>
2023-11-28 15:43:51 +0100

    Testing compatibility with customfields

--- lbplanner/db/install.php
+++ lbplanner/db/install.php
@@ -14,14 +14,32 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+use core_customfield\category_controller;
+use core_customfield\field_controller;
 use local_lbplanner\helpers\user_helper;
 use local_lbplanner\helpers\config_helper;
+use local_modcustomfields\customfield\mod_handler as mod_handler;
+use local_modcustomfields;
 
 defined('MOODLE_INTERNAL') || die;
 
+/**
+ * @throws coding_exception
+ * @throws moodle_exception
+ */
 function xmldb_local_lbplanner_install() {
     global $DB;
 
+    get_plugin_generator('local_lbplanner')->install();
+    if (in_array('modcustomfields', get_list_of_plugins('local'))) {
+        $handler = mod_handler::create();
+        $categorycontroller = category_controller::create(0, null, $handler);
+        $categorycontroller->save();
+        $categoryid = $categorycontroller->get_category()->get_id();
+        $fieldcontroller = field_controller::create(0, null, $categorycontroller);
+        $fieldcontroller->save();
+        $handler->setup_edit_page($fieldcontroller);
+    }
 }
 config_helper::set_default_active_year();
 
