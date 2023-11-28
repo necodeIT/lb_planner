a643af0179c5f3fb83c1c025a5f4e3bdcff68a0e
mcquenji <60017181+mcquenji@users.noreply.github.com>
2023-11-28 14:50:57 +0100

    LP-198-comment-analysis-results (#64)
    * added python script for starter
    
    * added analysis comment
    
    * removed bash from full report
    
    * fixed job failing after analysis
    
    * fixed "View annotated files" url
    
    * changed comment heading
    
    also noticed the commit sha in the title does not match the latest commit. i think i fixed that now
    
    * fixed "View annotated files" url II
    
    also fixed the commit sha for the unit test comment
    
    * fixed "View annotated files" url III
    
    * fixed "View annotated files" url IV
    
    * added documentation

---
+++ .github/workflows/construct_analysis_comment.py
@@ -0,0 +1,75 @@
+"""
+This script processes linting results and generates a formatted analysis report suitable to be posted as a comment.
+
+Usage:
+------
+```bash
+python script_name.py [LINTS_FILE] [COMMIT_SHA] [PR_NUMBER] [OUTPUT_FILE]
+```
+
+Parameters:
+LINTS_FILE: The file path containing the linting results.
+COMMIT_SHA: The SHA of the commit being analyzed.
+PR_NUMBER: The number of the PR being analyzed.
+OUTPUT_FILE: The file path where the generated comment will be saved.
+
+Example:
+--------
+```bash
+python script_name.py "lint_results.txt" "abcd1234" "http://example.com/pr" "report.md"
+```
+"""
+
+import sys
+
+LINTS_FILE = sys.argv[1]
+COMMIT_SHA = sys.argv[2]
+PR_NUMBER = sys.argv[3]
+OUTPUT_FILE = sys.argv[4]
+
+
+# Read the file
+with open(LINTS_FILE, "r") as f:
+    LINTS = f.read().splitlines()
+
+# Remove first line as it is always "Analyzing [package-name]..." and we don't want to include it in the comment
+LINTS.pop(0)
+
+# Remove empty lines
+LINTS = [line.strip() for line in LINTS if line]
+
+infos = 0
+warnings = 0
+errors = 0
+
+for line in LINTS:
+    if line.startswith("info"):
+        infos += 1
+    elif line.startswith("warning"):
+        warnings += 1
+    elif line.startswith("error"):
+        errors += 1
+
+comment = f"""
+# Analysis Report for {COMMIT_SHA}
+
+- **Infos**: {infos}
+- **Warnings**: {warnings}
+- **Errors**: {errors}
+
+<details>
+<summary>Click to see the full report</summary>
+
+```
+{chr(10).join(LINTS)}
+```
+
+</details>
+
+---
+
+[View annotated files]({PR_NUMBER}/files)
+"""
+
+with open(OUTPUT_FILE, "w") as f:
+    f.write(comment)
